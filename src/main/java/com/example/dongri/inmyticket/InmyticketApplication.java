1db8ef07ae87f353821e6a2022d05f31af834abc
DB <hdb151@naver.com>
2026-05-19 19:12:32 +0900

    깃허브 등록

---
+++ src/main/java/com/example/dongri/inmyticket/InmyticketApplication.java
@@ -0,0 +1,13 @@
+package com.example.dongri.inmyticket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InmyticketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InmyticketApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/dongri/inmyticket/InmyticketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.dongri.inmyticket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InmyticketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
