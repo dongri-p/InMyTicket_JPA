06439414e7bed546cb2d9be904e85e90dfbd1a6a
DB <hdb151@naver.com>
2026-05-27 19:10:36 +0900

    feat: MemberRepository 및 중복 검증 로직을 포함한 MemberService 구현 #3

---
+++ src/main/java/com/example/dongri/inmyticket/service/MemberService.java
@@ -0,0 +1,39 @@
+package com.example.dongri.inmyticket.service;
+
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.example.dongri.inmyticket.domain.Member;
+import com.example.dongri.inmyticket.repository.MemberRepository;
+
+import lombok.RequiredArgsConstructor;
+
+@Service
+@Transactional(readOnly = true)
+@RequiredArgsConstructor
+public class MemberService {
+
+    private final MemberRepository memberRepository;
+
+    @Transactional
+    public Long join(Member member) {
+        validateDuplicateMember(member);
+        memberRepository.save(member);
+        return member.getId();
+    }
+
+    private void validateDuplicateMember(Member member) {
+        memberRepository.findByEmail(member.getEmail())
+            .ifPresent(m -> {
+                throw new IllegalStateException("이미 존재하는 회원입니다.");
+            });
+    }
+
+    public Member findOne(Long memberId) {
+        return memberRepository.findById(memberId)
+                .orElseThrow(() -> new IllegalArgumentException("존재하지 않는 회원입니다."));
+
+    }
+     
+    
+}
